05b420861e5f7ff86420c3ce429b622ec4dd243c
oddiybol <oddiybola2019@gmail.com>
2022-05-19 16:24:42 +0600

    init

--- weather/urls.py
+++ weather/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from main.views import a
 urlpatterns = [
+    path('', a, name='a'),
     path('admin/', admin.site.urls),
 ]
